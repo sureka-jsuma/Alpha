97c6af16ef1db57aa2811215b46e1fd7c645585a
sureka-jsuma <jsuma5050@gmail.com>
2022-03-05 20:42:03 +0530

    validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,21 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article=Article.new
         #navigates to a page to fill the title and description. for temple
     end
 
     def create
         # render plain: params[:article] #{"title"=>"12344555", "description"=>""}
         @article=Article.new(params.require(:article).permit(:title,:description))
-        @article.save
-        # redirect_to article_path(@article) or
-        redirect_to @article
+        if @article.save
+          flash[:notice]="Article saved successfully"
+          # redirect_to article_path(@article) or
+          redirect_to @article
+        else
+            render 'new'
+        end
+        
     end
 
 end
