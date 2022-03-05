c6f4c046e8260016d89844ca93930e07ec037f3b
sureka-jsuma <jsuma5050@gmail.com>
2022-03-05 18:11:22 +0530

    Article listing [ articles#index ] added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @articles=Article.all
+    end
+
     def show
         # byebug
         @article=Article.find(params[:id])
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
  root "pages#home"
  get "about" , to: "pages#about"
 #  resources "articles" --> this one also worked
- resources :articles, only: [:show]
+ resources :articles, only: [:show, :index]
 end
