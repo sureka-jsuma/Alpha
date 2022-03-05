2f27e5fcfc2bad300662c8c49e571f9e4433153e
sureka-jsuma <jsuma5050@gmail.com>
2022-03-05 22:19:35 +0530

    delete [ articles#destroy ] added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  root "pages#home"
  get "about" , to: "pages#about"
 #  resources "articles" --> this one also worked
- resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+#  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+ resources :articles
 end
